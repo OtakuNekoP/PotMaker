cbf0fa33e56b7e5cd54e53ad05cc72a4e760978d
某10 <i@liub.org>
2016-02-28 11:02:02 +0800

    Update new.php

--- new.php
+++ new.php
@@ -1,33 +1,28 @@
 <?php
 $isview = false;
 $qq = $_POST["qq"];
-if(isset($_POST["qq"])){
+if(isset($qq)){
 	$isview = true; 
 }else{
 	$isview = false;
 }
-if($isview){ ?> 
-
-<?php
-$dst_path = 'background.png';
-$src_path = "http://q2.qlogo.cn/headimg_dl?bs=qq&dst_uin=" . $qq . "&spec=100";
-$dst = imagecreatefromstring(file_get_contents($dst_path));
-$src = imagecreatefromstring(file_get_contents($src_path));
-list($src_w, $src_h) = getimagesize($src_path);
-imagecopymerge($dst, $src, 200, 39, 0, 0, $src_w, $src_h, 100);
-$thumb = imagecreatetruecolor(120, 80);
-ImageCopyResized($thumb, $dst, 0, 0, 0, 0, 120, 80, 343, 229);
-list($dst_w, $dst_h, $dst_type) = getimagesize($dst_path);
-header('Content-Type: image/png');
-imagepng($thumb);
-imagedestroy($dst);
-imagedestroy($src);
-imagedestroy($thumb);
-?>
-
-
-<?php }else{ ?> 
-
+if($isview){
+	$dst_path = 'background.png';
+	$src_path = "http://q2.qlogo.cn/headimg_dl?bs=qq&dst_uin=" . $qq . "&spec=100";
+	$dst = imagecreatefromstring(file_get_contents($dst_path));
+	$src = imagecreatefromstring(file_get_contents($src_path));
+	list($src_w, $src_h) = getimagesize($src_path);
+	imagecopymerge($dst, $src, 200, 39, 0, 0, $src_w, $src_h, 100);
+	$thumb = imagecreatetruecolor(120, 80);
+	ImageCopyResized($thumb, $dst, 0, 0, 0, 0, 120, 80, 343, 229);
+	list($dst_w, $dst_h, $dst_type) = getimagesize($dst_path);
+	header('Content-Type: image/png');
+	imagepng($thumb);
+	imagedestroy($dst);
+	imagedestroy($src);
+	imagedestroy($thumb);
+}else{
+	echo '
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" " http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
 <html xmlns=" http://www.w3.org/1999/xhtml"> 
 <head> 
@@ -43,6 +38,6 @@ imagedestroy($thumb);
 <input type="text" name="qq" /> <input type="submit" value="丢锅！" /> 
 </form>  
 </body> 
-</html> 
-<?php 
-} ?>
+</html> ';
+} 
+?>
